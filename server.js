9288ec7440e2c8bb36d7ce25855a2dfd0295d5c1
Bryson <brysonthill@gmail.com>
2016-03-13 19:29:50 -0700

    Another attempt at redirects

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 var express = require('express');
 var compression = require('compression');
 var app = express();
-app.use(compression());
-app.use(express.static(__dirname + '/public'));
 
-app.all('/*', function(req, res, next) {
+app.all('*', function(req, res, next) {
+  console.log(req.headers);
   if (req.headers.host.match(/^www/) !== null ) {
     res.redirect('http://' + req.headers.host.replace(/^www\./, '') + req.url);
   } else {
     next();
   }
-})
+});
+
+app.use(compression());
+app.use(express.static(__dirname + '/public'));
 
 app.listen(80);
 console.log("Running on port 80...");
